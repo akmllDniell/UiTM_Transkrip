7138cbc0c0810e94120a3e1064c0c859f669a055
Arif Asyraf <arifasyraf70@gmail.com>
2022-12-06 19:42:22 +0800

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -27,12 +27,7 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="/" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
+      
     </ul>
 
     <!-- Right navbar links -->
@@ -247,12 +242,11 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank ilyif.9lyvgikugvi7</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="/">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+              <li class="breadcrumb-item active">Main page</li>
             </ol>
           </div>
         </div>
@@ -266,23 +260,157 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Log in as: {{$user->name}}</h3>
+          <h3 class="card-title">Log in as : {{$user->name}}</h3>
+
+
+          {{-- UNTUK ADMIN CEK PELAJAR --}}
+
 
           <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
+            
             <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
               <i class="fas fa-times"></i>
             </button>
           </div>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+          
         </div>
+
+        <section class="content">
+          <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+
+
+            {{-- PERTAMA MULA --}}
+
+            <div class="row">
+              <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <center><h2><b>6969</b></h2></center>
+                    {{-- <h3>{{$useradmin}}</h3> --}}
+    
+                    <p><center>JUMLAH ADMIN BERDAFTAR</center></p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-bag"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+              {{-- PERTAMA HABIS --}}
+
+              {{-- KEDUA MULA --}}
+              <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                  <div class="inner">
+                    <center><h2><b>9696</b></h2></center>
+    
+                    <p><center>JUMLAH PELAJAR DAFTAR</center></p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+              {{-- KEDUA HABIS --}}
+
+              {{-- KETIGA MULA --}}
+              <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                  <div class="inner">
+                    <h3><center><H2><b>420</b></H2></center></h3>
+    
+                    <p><center>JUMLAH USER</center></p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+              {{-- KETIGA HABIS --}}
+
+              {{-- KEEMPAT MULA --}}
+              <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                  <div class="inner">
+                    <h3><center><H2><b>8</b></H2></center></h3>
+    
+                    <p><center>JUMLAH AKTIVITI</center></p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+              {{-- KEEMPAT HABIS --}}
+
+              {{-- KALENDER BUKA  --}}
+              <section class="col-lg-5 connectedSortable">
+              <div class="card bg-gradient-success">
+                <div class="card-header border-0">
+  
+                  <h3 class="card-title">
+                    <i class="far fa-calendar-alt"></i>
+                    kalendar 
+                  </h3>
+                  <!-- tools card -->
+                  <div class="card-tools">
+                    <!-- button with a dropdown -->
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
+                        <i class="fas fa-bars"></i>
+                      </button>
+                      <div class="dropdown-menu" role="menu">
+                       
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">View calendar</a>
+                      </div>
+                    </div>
+                    <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                  <!-- /. tools -->
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body pt-0">
+                  <!--The calendar -->
+                  <div id="calendar" style="width: 100%"></div>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              </section>
+
+              {{-- KALENDER TUTUP --}}
+            </div>
+          </div>
+
+          
+        </section>
+
+
+
+        {{-- UNTUK ADMIN CEK PELAJAR TUTUP --}}
+
+
+
+
+
         <!-- /.card-body -->
         <div class="card-footer">
-          Footer
+       
         </div>
         <!-- /.card-footer-->
       </div>
@@ -299,34 +427,7 @@
     </div>
 @endif
       <div class="card card-info">
-              <div class="card-header">
-                <h3 class="card-title">TAMBAH AKTIVITI</h3>
-              </div>
-              <div class="card-body">
-                <!-- Color Picker -->
-                <div class="form-group">
-                  <label>AKTIVITI CODE:</label>
-                  <input name="AktivitiCode" id="AktivitiCode" type="text" class="form-control my-colorpicker1 colorpicker-element" data-colorpicker-id="1" data-original-title="" title="">
-                </div>
-                <!-- /.form group -->
-
-                <!-- Color Picker -->                
-                <div class="form-group">
-                  <label>NAMA AKTIVITI:</label>
-
-                  <div class="input-group my-colorpicker2 colorpicker-element" data-colorpicker-id="2">
-                    <input name="NamaAktiviti" id="NamaAktiviti" type="text" class="form-control" >
-
-                    <div class="input-group-append">
-                      <span class="input-group-text"><i class="fas fa-square"></i></span>
-                    </div>
-                  </div>
-                  <button type="submit" class="btn btn-block bg-gradient-success">SAVE</button>
-                  <!-- /.input group -->
-                </div>
-                </form>
-                <!-- /.form group -->
-
+              
                 
                     <!-- /.input group -->
                   </div>
