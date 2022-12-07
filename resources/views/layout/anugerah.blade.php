39a8c28ee0265a3b9d146866eba3490f930d2fab
AKMAL DANIEL BIN MOHD BADROL HISHAM <akmaldaniel452002@gmail.com>
2022-12-07 08:44:55 +0800

    update anugerah.php

--- resources/views/layout/anugerah.blade.php
+++ resources/views/layout/anugerah.blade.php
@@ -239,24 +239,6 @@
     </div>
     <!-- /.sidebar -->
   </aside>
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>DataTables</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
 
 
   <!-- Content Wrapper. Contains page content -->
