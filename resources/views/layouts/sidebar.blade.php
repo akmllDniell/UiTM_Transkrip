828f9f7500ba5aee281bb295652ef1dfd8c279c8
Arif Asyraf <arifasyraf70@gmail.com>
2022-12-21 00:33:24 +0800

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -87,7 +87,7 @@
               </a>
               </li>
               <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="palapes" class="nav-link">
               <i class="far fa-dot-circle nav-icon"></i>
               <p>PALAPES</p>
               </a>
@@ -109,7 +109,7 @@
               </ul>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="profiling/anugerah" class="nav-link">
+                <a href="anugerah" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>ANUGERAH / SIJIL</p>
                 </a>
